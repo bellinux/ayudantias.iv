a03c4eb0a4dc93372fa2e93c445d9f209608315f
JaviPeace <javiera.azcar@uc.cl>
2025-10-23 21:32:07 -0300

    protoobject

---
+++ 7-protobject/ejemplo-1/config.js
@@ -0,0 +1,15 @@
+Protobject.setProduction(true)
+Protobject.initialize(
+[
+  { 
+    name: "Button",
+    page: "buttons.html",
+    debug: "local",
+  },
+  { 
+    name: "Graphs",
+    page: "index.html",
+    main: true,
+    debug: "master",
+  }
+]);
